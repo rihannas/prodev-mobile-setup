74e7f177ba89d4284c110670fe752b6141b72c2d
rihanas <rihanna.socials@gmail.com>
2025-11-05 23:47:23 +0300

    task 3 fix

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -15,14 +15,14 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <ImageBackground
-          source={require('@/assets/images/background_image.jpg')}
+          source={require('@/assets/images/background-image.png')}
           style={styles.background}
           resizeMode='cover'
         >
           <View style={styles.container}>
             {/* Company Logo */}
             <View style={styles.companyLogo}>
-              <Image source={require('@/assets/images/logo.png')} />
+              <Image source={require('@/assets/images/Logo.png')} />
             </View>
 
             {/* Text Group */}
