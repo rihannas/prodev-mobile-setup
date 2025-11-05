7f0062ed59638d36e7a6925d7dc6fb45a03e1465
rihanas <rihanna.socials@gmail.com>
2025-11-06 00:04:39 +0300

    task 4

---
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { styles } from '@/styles'; // use relative path if alias fails: "../../styles"
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* Navigation / Header */}
+        <View style={styles.navGroup}>
+          <Ionicons
+            name='arrow-back'
+            size={25}
+            color='#000'
+          />
+          <Image source={require('@/assets/images/logo.png')} />
+        </View>
+
+        {/* Title */}
+        <Text style={styles.largeText}>Sign in to your</Text>
+        <Text style={styles.largeText}>Account</Text>
+        <Text style={styles.smallText}>
+          Enter your email and password to sign in.
+        </Text>
+
+        {/* Form */}
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.placeholderText}>Email</Text>
+            <TextInput
+              keyboardType='email-address'
+              style={styles.inputField}
+              placeholder='Enter your email'
+            />
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Password</Text>
+            <View style={styles.passwordGroup}>
+              <TextInput
+                style={{ flex: 1 }}
+                placeholder='Enter your password'
+                secureTextEntry
+              />
+              <FontAwesome
+                name='eye-slash'
+                size={24}
+                color='#7E7B7B'
+              />
+            </View>
+          </View>
+
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </View>
+
+        {/* Sign In Button */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+
+        {/* Divider */}
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        {/* Social Media Buttons */}
+        <View style={styles.socialMediaButtonGroup}>
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}
+            >
+              <Image source={require('@/assets/images/google.png')} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}
+            >
+              <Image source={require('@/assets/images/facebook.png')} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* Bottom Text */}
+        <View style={styles.subTextGroup}>
+          <Text style={styles.subText}>Don't have an account?</Text>
+          <Text style={styles.subTextJoin}>Join now</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
